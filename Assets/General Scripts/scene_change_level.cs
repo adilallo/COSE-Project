5c1a7f10b27d85fbf1d181deac2e4448a06bc674
COSEresearchGroup <coseresearchgroup@gmail.com>
2024-05-08 13:18:31 +0200

    All interactivity uploaded for Jiawen's room
    Now, all elements are in the room. Waiting on approval from Inge and Jiawen

---
+++ Assets/General Scripts/scene_change_level.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class scene_change_level : MonoBehaviour
+{
+   
+    public string LoadLevel;
+
+  void OnTriggerEnter(Collider other)
+     
+    {
+        if(other.tag == "Player")
+
+        { 
+            SceneManager.LoadScene(LoadLevel);
+        }
+        
+    }
+}
