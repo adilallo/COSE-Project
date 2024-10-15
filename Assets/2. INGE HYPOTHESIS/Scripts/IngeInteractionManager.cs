aaf696cb49da595575ef591e828611b8a7831a6a
Andy DiLallo <39313955+adilallo@users.noreply.github.com>
2024-10-15 08:48:39 -0600

    Hypothesis 4 completed

--- Assets/2. INGE HYPOTHESIS/Scripts/IngeInteractionManager.cs
+++ Assets/2. INGE HYPOTHESIS/Scripts/IngeInteractionManager.cs
@@ -12,13 +12,15 @@ public class IngeInteractionManager : MonoBehaviour
     [SerializeField] private GameObject[] ingameTxt;
     [SerializeField] private GameObject[] citations;
     [SerializeField] private GameObject[] coins;
-    [SerializeField] private GameObject[] animations;
     [SerializeField] private GameObject hypothesis1Model;
-    [SerializeField] private GameObject hypothesis2Model;
+    [SerializeField] private GameObject hypothesis2Icons;
+    [SerializeField] private GameObject[] hypothesis2Animations;
     [SerializeField] private GameObject hypothesis3Model;
     [SerializeField] private GameObject hypothesis3Couplings;
-    [SerializeField] private GameObject[] couplingAnimations;
-    [SerializeField] private GameObject[] couplingNumbers;
+    [SerializeField] private GameObject[] hypothesis3Animations;
+    [SerializeField] private GameObject[] hypothesis3Numbers;
+    [SerializeField] private GameObject hypothesis4Buzzwords;
+    [SerializeField] private GameObject[] hypothesis4Animations;
 
     private void OnEnable()
     {
@@ -51,7 +53,7 @@ public class IngeInteractionManager : MonoBehaviour
                 break;
             case "INGE_SPHERE_HYPOTHESIS_3_LOC_ID":
                 hypothesisInteraction.CurrentStateIndex = 3;
-                hypothesis2Model.SetActive(false);
+                hypothesis2Icons.SetActive(false);
                 hypothesisInteraction.MoveModel(hypothesis1Model);
                 break;
             case "INGE_SPHERE_HYPOTHESIS_4_LOC_ID":
@@ -196,6 +198,42 @@ public class IngeInteractionManager : MonoBehaviour
                 textInteraction.ActivateLayerText(textKey);
                 Hypothesis3("INGE_LAYER_COUPLING_9_LOC_ID");
                 break;
+            case "INGE_LAYER_BACKGROUND_ELEMENTS_LOC_ID":
+                textInteraction.ActivateLayerText(textKey);
+                Hypothesis4("INGE_LAYER_BACKGROUND_ELEMENTS_LOC_ID");
+                break;
+            case "INGE_LAYER_HYPOTHESIS_4_L1_LOC_ID":
+                textInteraction.ActivateLayerText(textKey);
+                Hypothesis4("INGE_LAYER_HYPOTHESIS_4_L1_LOC_ID");
+                break;
+            case "INGE_LAYER_HYPOTHESIS_4_L2_LOC_ID":
+                textInteraction.ActivateLayerText(textKey);
+                Hypothesis4("INGE_LAYER_HYPOTHESIS_4_L2_LOC_ID");
+                break;
+            case "INGE_LAYER_HYPOTHESIS_4_L3_LOC_ID":
+                textInteraction.ActivateLayerText(textKey);
+                Hypothesis4("INGE_LAYER_HYPOTHESIS_4_L3_LOC_ID");
+                break;
+            case "INGE_LAYER_SHRINK_MULTIPLY_LOC_ID":
+                textInteraction.ActivateLayerText(textKey);
+                Hypothesis4("INGE_LAYER_SHRINK_MULTIPLY_LOC_ID");
+                break;
+            case "INGE_LAYER_HYPOTHESIS_4_L6_LOC_ID":
+                textInteraction.ActivateLayerText(textKey);
+                Hypothesis4("INGE_LAYER_HYPOTHESIS_4_L6_LOC_ID");
+                break;
+            case "INGE_LAYER_HYPOTHESIS_4_L7_LOC_ID":
+                textInteraction.ActivateLayerText(textKey);
+                Hypothesis4("INGE_LAYER_HYPOTHESIS_4_L7_LOC_ID");
+                break;
+            case "INGE_LAYER_HYPOTHESIS_4_L8_LOC_ID":
+                textInteraction.ActivateLayerText(textKey);
+                Hypothesis4("INGE_LAYER_HYPOTHESIS_4_L8_LOC_ID");
+                break;
+            case "INGE_LAYER_HYPOTHESIS_4_L9_LOC_ID":
+                textInteraction.ActivateLayerText(textKey);
+                Hypothesis4("INGE_LAYER_HYPOTHESIS_4_L9_LOC_ID");
+                break;
         }
     }
 
@@ -214,139 +252,224 @@ public class IngeInteractionManager : MonoBehaviour
         switch(currentIndex)
         {
             case 2:
-                hypothesis2Model.SetActive(true);
+                hypothesis2Icons.SetActive(true);
                 break;
             case 3:
                 hypothesis1Model.SetActive(false);
                 hypothesis3Model.SetActive(true);
                 hypothesis3Couplings.SetActive(true);
                 break;
+            case 4:
+                hypothesis4Buzzwords.SetActive(true);
+                break;
         }
     }
 
     private void Hypothesis2(string layerText)
     {
-        foreach (GameObject animation in animations)
+        foreach (GameObject animation in hypothesis2Animations)
         {
             animation.SetActive(false);
         }
         switch (layerText)
         {
             case "INGE_LAYER_ALL_DIRECTIONS_ARROW_LOC_ID":
-                animations[0].SetActive(true);
+                hypothesis2Animations[0].SetActive(true);
                 break;
             case "INGE_LAYER_ALL_ARROW_IN_LOC_ID":
-                animations[1].SetActive(true);
+                hypothesis2Animations[1].SetActive(true);
                 break;
             case "INGE_LAYER_LATERAL_ARROW_LOC_ID":
-                animations[2].SetActive(true);
+                hypothesis2Animations[2].SetActive(true);
                 break;
             case "INGE_LAYER_MAGNIFYING_GLASS_LOC_ID":
-                animations[3].SetActive(true);
+                hypothesis2Animations[3].SetActive(true);
                 break;
             case "INGE_LAYER_MARKER_LOC_ID":
-                animations[4].SetActive(true);
+                hypothesis2Animations[4].SetActive(true);
                 break;
             case "INGE_LAYER_MEGAPHONE_LOC_ID":
-                animations[5].SetActive(true);
+                hypothesis2Animations[5].SetActive(true);
                 break;
             case "INGE_LAYER_PENCIL_LOC_ID":
-                animations[6].SetActive(true);
+                hypothesis2Animations[6].SetActive(true);
                 break;
             case "INGE_LAYER_UP_DOWN_ARROW_LOC_ID":
-                animations[7].SetActive(true);
+                hypothesis2Animations[7].SetActive(true);
                 break;
             case "INGE_LAYER_DOUBLE_CLICK_ARROW_LOC_ID":
-                animations[8].SetActive(true);
+                hypothesis2Animations[8].SetActive(true);
                 break;
             case "INGE_LAYER_RESIZE_LOC_ID":
-                animations[9].SetActive(true);
+                hypothesis2Animations[9].SetActive(true);
                 break;
         }
     }
 
     private void Hypothesis3(string layerText)
     {
-        foreach (GameObject couplingNumber in couplingNumbers)
+        foreach (GameObject couplingNumber in hypothesis3Numbers)
         {
             couplingNumber.SetActive(false);
         }
-        foreach (GameObject couplingAnimation in couplingAnimations)
+        foreach (GameObject couplingAnimation in hypothesis3Animations)
         {
             couplingAnimation.SetActive(false);
         }
         switch(layerText)
         {
             case "INGE_LAYER_COUPLING_1_LOC_ID":
-                couplingNumbers[0].SetActive(true);
-                couplingNumbers[4].SetActive(true);
-                couplingNumbers[5].SetActive(true);
-                couplingNumbers[6].SetActive(true);
-                couplingNumbers[7].SetActive(true);
-                couplingNumbers[8].SetActive(true);
-                couplingNumbers[11].SetActive(true);
-                couplingNumbers[13].SetActive(true);
-                couplingNumbers[16].SetActive(true);
-                couplingAnimations[0].SetActive(true);
+                hypothesis3Numbers[0].SetActive(true);
+                hypothesis3Numbers[4].SetActive(true);
+                hypothesis3Numbers[5].SetActive(true);
+                hypothesis3Numbers[6].SetActive(true);
+                hypothesis3Numbers[7].SetActive(true);
+                hypothesis3Numbers[8].SetActive(true);
+                hypothesis3Numbers[11].SetActive(true);
+                hypothesis3Numbers[13].SetActive(true);
+                hypothesis3Numbers[16].SetActive(true);
+                hypothesis3Animations[0].SetActive(true);
                 break;
             case "INGE_LAYER_COUPLING_2_LOC_ID":
-                couplingNumbers[0].SetActive(true);
-                couplingNumbers[4].SetActive(true);
-                couplingNumbers[5].SetActive(true);
-                couplingNumbers[6].SetActive(true);
-                couplingNumbers[7].SetActive(true);
-                couplingNumbers[8].SetActive(true);
-                couplingNumbers[11].SetActive(true);
-                couplingNumbers[13].SetActive(true);
-                couplingNumbers[16].SetActive(true);
-                couplingAnimations[1].SetActive(true);
+                hypothesis3Numbers[0].SetActive(true);
+                hypothesis3Numbers[4].SetActive(true);
+                hypothesis3Numbers[5].SetActive(true);
+                hypothesis3Numbers[6].SetActive(true);
+                hypothesis3Numbers[7].SetActive(true);
+                hypothesis3Numbers[8].SetActive(true);
+                hypothesis3Numbers[11].SetActive(true);
+                hypothesis3Numbers[13].SetActive(true);
+                hypothesis3Numbers[16].SetActive(true);
+                hypothesis3Animations[1].SetActive(true);
                 break;
             case "INGE_LAYER_COUPLING_3_LOC_ID":
-                couplingNumbers[6].SetActive(true);
-                couplingNumbers[16].SetActive(true);
-                couplingAnimations[2].SetActive(true);
+                hypothesis3Numbers[6].SetActive(true);
+                hypothesis3Numbers[16].SetActive(true);
+                hypothesis3Animations[2].SetActive(true);
                 break;
             case "INGE_LAYER_COUPLING_4_LOC_ID":
-                couplingNumbers[1].SetActive(true);
-                couplingNumbers[9].SetActive(true);
-                couplingNumbers[10].SetActive(true);
-                couplingNumbers[12].SetActive(true);
-                couplingNumbers[15].SetActive(true);
-                couplingAnimations[3].SetActive(true);
+                hypothesis3Numbers[1].SetActive(true);
+                hypothesis3Numbers[9].SetActive(true);
+                hypothesis3Numbers[10].SetActive(true);
+                hypothesis3Numbers[12].SetActive(true);
+                hypothesis3Numbers[15].SetActive(true);
+                hypothesis3Animations[3].SetActive(true);
                 break;
             case "INGE_LAYER_COUPLING_5_LOC_ID":
-                couplingNumbers[1].SetActive(true);
-                couplingNumbers[2].SetActive(true);
-                couplingAnimations[4].SetActive(true);
+                hypothesis3Numbers[1].SetActive(true);
+                hypothesis3Numbers[2].SetActive(true);
+                hypothesis3Animations[4].SetActive(true);
                 break;
             case "INGE_LAYER_COUPLING_6_LOC_ID":
-                couplingNumbers[2].SetActive(true);
-                couplingNumbers[14].SetActive(true);
-                couplingNumbers[4].SetActive(true);
-                couplingNumbers[8].SetActive(true);
-                couplingNumbers[9].SetActive(true);
-                couplingNumbers[13].SetActive(true);
-                couplingAnimations[5].SetActive(true);
+                hypothesis3Numbers[2].SetActive(true);
+                hypothesis3Numbers[14].SetActive(true);
+                hypothesis3Numbers[4].SetActive(true);
+                hypothesis3Numbers[8].SetActive(true);
+                hypothesis3Numbers[9].SetActive(true);
+                hypothesis3Numbers[13].SetActive(true);
+                hypothesis3Animations[5].SetActive(true);
                 break;
             case "INGE_LAYER_COUPLING_7_LOC_ID":
-                couplingNumbers[5].SetActive(true);
-                couplingNumbers[13].SetActive(true);
-                couplingAnimations[6].SetActive(true);
+                hypothesis3Numbers[5].SetActive(true);
+                hypothesis3Numbers[13].SetActive(true);
+                hypothesis3Animations[6].SetActive(true);
                 break;
             case "INGE_LAYER_COUPLING_8_LOC_ID":
-                couplingNumbers[3].SetActive(true);
-                couplingNumbers[12].SetActive(true);
-                couplingNumbers[14].SetActive(true);
-                couplingNumbers[0].SetActive(true);
-                couplingNumbers[6].SetActive(true);
-                couplingAnimations[7].SetActive(true);
+                hypothesis3Numbers[3].SetActive(true);
+                hypothesis3Numbers[12].SetActive(true);
+                hypothesis3Numbers[14].SetActive(true);
+                hypothesis3Numbers[0].SetActive(true);
+                hypothesis3Numbers[6].SetActive(true);
+                hypothesis3Animations[7].SetActive(true);
                 break;
             case "INGE_LAYER_COUPLING_9_LOC_ID":
-                foreach (GameObject couplingNumber in couplingNumbers)
+                foreach (GameObject couplingNumber in hypothesis3Numbers)
                 {
                     couplingNumber.SetActive(true);
                 }
-                couplingAnimations[8].SetActive(true);
+                hypothesis3Animations[8].SetActive(true);
+                break;
+        }
+    }
+
+    private void Hypothesis4(string layerText)
+    {
+        foreach (GameObject couplingNumber in hypothesis3Numbers)
+        {
+            couplingNumber.SetActive(false);
+        }
+        foreach (GameObject animations in hypothesis4Animations)
+        {
+            animations.SetActive(false);
+        }
+        switch (layerText)
+        {
+            case "INGE_LAYER_BACKGROUND_ELEMENTS_LOC_ID":
+                hypothesis3Numbers[0].SetActive(true);
+                hypothesis3Numbers[1].SetActive(true);
+                hypothesis3Numbers[2].SetActive(true);
+                hypothesis3Numbers[4].SetActive(true);
+                hypothesis3Numbers[5].SetActive(true);
+                hypothesis3Numbers[8].SetActive(true);
+                hypothesis3Numbers[15].SetActive(true);
+                hypothesis4Animations[0].SetActive(true);
+                break;
+            case "INGE_LAYER_HYPOTHESIS_4_L1_LOC_ID":
+                hypothesis3Numbers[0].SetActive(true);
+                hypothesis3Numbers[2].SetActive(true);
+                hypothesis3Numbers[4].SetActive(true);
+                hypothesis3Numbers[5].SetActive(true);
+                hypothesis3Numbers[6].SetActive(true);
+                hypothesis3Numbers[7].SetActive(true);
+                hypothesis3Numbers[8].SetActive(true);
+                hypothesis3Numbers[11].SetActive(true);
+                hypothesis3Numbers[13].SetActive(true);
+                hypothesis3Numbers[16].SetActive(true);
+                hypothesis4Animations[1].SetActive(true);
+                break;
+            case "INGE_LAYER_HYPOTHESIS_4_L2_LOC_ID":
+                hypothesis3Numbers[6].SetActive(true);
+                hypothesis3Numbers[2].SetActive(true);
+                hypothesis4Animations[2].SetActive(true);
+                break;
+            case "INGE_LAYER_HYPOTHESIS_4_L3_LOC_ID":
+                hypothesis3Numbers[7].SetActive(true);
+                hypothesis3Numbers[9].SetActive(true);
+                hypothesis4Animations[3].SetActive(true);
+                break;
+            case "INGE_LAYER_SHRINK_MULTIPLY_LOC_ID":
+                hypothesis3Numbers[7].SetActive(true);
+                hypothesis3Numbers[6].SetActive(true);
+                hypothesis3Numbers[5].SetActive(true);
+                hypothesis3Numbers[4].SetActive(true);
+                hypothesis3Numbers[11].SetActive(true);
+                hypothesis3Numbers[10].SetActive(true);
+                hypothesis3Numbers[9].SetActive(true);
+                hypothesis3Numbers[8].SetActive(true);
+                hypothesis4Animations[4].SetActive(true);
+                break;
+            case "INGE_LAYER_HYPOTHESIS_4_L6_LOC_ID":
+                hypothesis3Numbers[3].SetActive(true);
+                hypothesis3Numbers[17].SetActive(true);
+                hypothesis4Animations[5].SetActive(true);
+                break;
+            case "INGE_LAYER_HYPOTHESIS_4_L7_LOC_ID":
+                hypothesis3Numbers[6].SetActive(true);
+                hypothesis3Numbers[2].SetActive(true);
+                hypothesis4Animations[6].SetActive(true);
+                break;
+            case "INGE_LAYER_HYPOTHESIS_4_L8_LOC_ID":
+                hypothesis3Numbers[2].SetActive(true);
+                hypothesis3Numbers[6].SetActive(true);
+                hypothesis3Numbers[7].SetActive(true);
+                hypothesis3Numbers[9].SetActive(true);
+                hypothesis4Animations[7].SetActive(true);
+                break;
+            case "INGE_LAYER_HYPOTHESIS_4_L9_LOC_ID":
+                hypothesis3Numbers[1].SetActive(true);
+                hypothesis3Numbers[10].SetActive(true);
+                hypothesis3Numbers[15].SetActive(true);
+                hypothesis4Animations[8].SetActive(true);
                 break;
         }
     }
--- Assets/2. INGE HYPOTHESIS/Scripts/IngeTextInteraction.cs
+++ Assets/2. INGE HYPOTHESIS/Scripts/IngeTextInteraction.cs
@@ -11,33 +11,4 @@ public class IngeTextInteraction : TextInteraction
     {
         base.DeactivateAllTexts();
     }
-
-        /* protected override void OnEnable()
-         {
-             base.OnEnable();
-             HypothesisLayerInteraction.OnLayerMoved += ActivateText;
-             HypothesisLayerInteraction.OnTextLayerClicked += ActivateText;
-             DiagramManager.Instance.OnDiagramElementClicked += HandleDiagramElementClicked;
-             HypothesisInteraction.OnCouplingActivated += ActivateCouplingText;
-         }
-
-         protected override void OnDisable()
-         {
-             base.OnDisable();
-             HypothesisLayerInteraction.OnLayerMoved -= ActivateText;
-             HypothesisLayerInteraction.OnTextLayerClicked -= ActivateText;
-             DiagramManager.Instance.OnDiagramElementClicked -= HandleDiagramElementClicked;
-             HypothesisInteraction.OnCouplingActivated -= ActivateCouplingText;
-         }
-
-         public void ActivateCouplingText(string couplingType)
-         {
-             this._text.text = couplingType;
-         }
-
-         private void HandleDiagramElementClicked(DiagramElement element)
-         {
-             Debug.Log($"Diagram element clicked: {element.name}");
-             DeactivateAllTexts();
-         }*/
 }
